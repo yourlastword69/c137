8fda961f56c70ed3fc3ea763f5cf9065e9b6a751
yourlastword69 <151046862+yourlastword69@users.noreply.github.com>
2024-08-22 19:45:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function preload(){
+img=loadImage("dog_cat.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640, 420);
+    canvas.center();
+}
+
+img = "";
+
+function draw(){
+    image(img, 0, 0, 640, 420);
+    fill("#FF0000");
+    text("Dog", 45, 75);
+    noFill();
+    stroke("#FF0000");
+    rect(30, 60, 450, 350)
+
+    fill("#FF0000")
+    text("Cat", 320, 120);
+    noFill();
+    stroke("#FF0000");
+    rect(300, 90, 270, 320);
+}
